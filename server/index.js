e0f33800d67e584845235f96439352f7abd820f9
WebDev Jones <webdeveloper.jones@gmail.com>
2023-02-21 16:59:09 -0500

    plz socket io

--- server/index.js
+++ server/index.js
@@ -82,4 +82,4 @@ const PORT = process.env.PORT || 3001
 
 
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
