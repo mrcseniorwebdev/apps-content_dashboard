c08d17fcfa94b6137175ec28ce8b35efa417bbec
WebDev Jones <webdeveloper.jones@gmail.com>
2023-03-08 11:51:24 -0500

    added functionality to clean the database of anything older than 3 days

--- server/utils/get_content.js
+++ server/utils/get_content.js
@@ -1,13 +1,22 @@
 const pool = require('../utils/db')
 // const format = require('date-fns/format')
 const { formatInTimeZone } = require('date-fns-tz')
+const subDays = require('date-fns/subDays')
+
 const fromUnixTime = require('date-fns/fromUnixTime')
+const getUnixTime = require('date-fns/getUnixTime')
 
 const getData = async () => {
     let rows
     let noData = false
     try {
+        //generate unix time date 3 days prior from this point
+        //then delete anything in the queue that is less than that
+        const threeDaysAgo = subDays(new Date(), 3)
+        const threeDaysAgoUnixTime = getUnixTime(threeDaysAgo)
+
         conn = await pool.getConnection()
+        rows = await conn.query('DELETE FROM content_queue WHERE unix_time < ?', [threeDaysAgoUnixTime])
         rows = await conn.query('SELECT * FROM content_queue ORDER BY unix_time DESC')
     }
     catch (err) {
